40682a596ce5a9c8e0e93016456b41e897e3dec4
dgdevanshi <dgdevanshi@gmail.com>
2022-10-18 15:07:32 +0530

    nagivation to home page fixed

--- src/routes/home.js
+++ src/routes/home.js
@@ -6,7 +6,7 @@ const router = exp.Router();
 const rootDir = require('../util/path');
 
 router.get('/home' , (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'index.html'));
+    res.sendFile(path.join(rootDir, 'views', 'home.html'));
 });
 
 module.exports = router;
